19b7667f3609f10b8bd844d0bf8bd3e7f0b14761
rlraul <raul.cardoso@outlook.com>
2021-03-21 17:10:44 -0300

    Criado projeto

---
+++ src/main/java/br/edu/utfpr/atividadeAula4/AtvidadeAula4Application.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.atividadeAula4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtvidadeAula4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtvidadeAula4Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/utfpr/atividadeAula4/AtvidadeAula4ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.atividadeAula4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtvidadeAula4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
